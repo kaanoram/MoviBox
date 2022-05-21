c153368431731c0dab5337b2f0dc4c1f4a93b056
Kaan Oram <58760678+kaanoram@users.noreply.github.com>
2022-05-21 11:35:45 -0500

    fixed bugs

--- watch.php
+++ watch.php
@@ -16,20 +16,20 @@ $upNextVideo = VideoProvider::getUpNext($con, $video);
         </button>
         <h1><?php echo $video->getTitle(); ?></h1>
     </div>
-    <div class="videoControls upNext">
-        <button>
+    <div class="videoControls upNext" style="display:none;">
+        <button onclick="restartVideo()">
             <i class="fas fa-redo"></i>
         </button>
         <div class = "upNextContainer">
             <h2>Up next:</h2>
             <h3><?php echo $upNextVideo->getTitle(); ?></h3>
             <h3><?php echo $upNextVideo->getSeasonAndEpisode(); ?></h3>
-            <button class="playNext">
+            <button class="playNext" onclick = 'watchVideo(<?php echo $upNextVideo->getId();?>)'>
                 <i class="fas fa-play"></i> Play
             </button>
         </div>
     </div>
-    <video controls autoplay>
+    <video controls autoplay onended = "showUpNext();">
         <source src='<?php echo $video->getFilePath(); ?>' type="video/mp4">
     </video>
 </div>
